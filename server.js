7c9a2a9d1e5be395fce9206070ef91da0fd39173
PALLAVI NISHTALA <pallavi.nishtala@in.ibm.com>
2018-02-15 13:16:17 +0530

    added

--- server.js
+++ server.js
@@ -60,6 +60,16 @@ app.get('/bad',(req,res)=>{
   });
 
 })
+
+
+app.get('/projects',(req,res)=>{
+ //res.send('About page')
+ res.render('projects.hbs',{
+   pageTitle : 'Projects porfolio!!!'
+  // currentYear: new Date().getFullYear()
+ });
+
+})
 app.listen(port,()=>{
 console.log(`server is up on port ${port}`);
 
